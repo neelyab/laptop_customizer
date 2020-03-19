128f93159ce5195720724c3e082c3018fd1f04e8
Amanda <neelyab@vcu.edu>
2020-03-19 16:07:04 -0400

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Form from '../src/Form/Form';
+import CartItems from '../src/CartItems/CartItems';
+import Total from '../src/Total/Total';
 
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
@@ -44,56 +47,7 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
+    
 
     return (
       <div className="App">
@@ -101,19 +55,21 @@ class App extends Component {
           <h1>ELF Computing | Laptops</h1>
         </header>
         <main>
-          <form className="main__form">
-            <h2>Customize your laptop</h2>
-            {features}
-          </form>
+          <Form 
+          features={this.props.features}
+          USCurrencyFormat={USCurrencyFormat}
+          selected={this.state.selected}
+          handleUpdate={this.updateFeature}
+          />
           <section className="main__summary">
             <h2>Your cart</h2>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
+            < CartItems
+            selected={this.state.selected}
+            USCurrencyFormat={USCurrencyFormat} />
+            <Total 
+            selected={this.state.selected}
+            USCurrencyFormat={USCurrencyFormat}
+            />
           </section>
         </main>
       </div>
